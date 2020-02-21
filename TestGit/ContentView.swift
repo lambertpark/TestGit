4b65de9a24431e6829a467de249305a62b3ca30f
lambert.park <lambert.park@lgepartner.com>
2020-02-21 14:15:00 +0900

    Create project.

---
+++ TestGit/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  TestGit
+//
+//  Created by JongPil Park on 2020/02/21.
+//  Copyright © 2020 JongPil Park. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
